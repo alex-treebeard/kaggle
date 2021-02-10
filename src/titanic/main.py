e0f631f6e1f34e8c192adb01f3ed68ed1dc482f9
alex-treebeard <alex@treebeard.io>
2021-02-10 15:00:57 +0000

    more fields

--- src/titanic/main.py
+++ src/titanic/main.py
@@ -1,10 +1,8 @@
-import numpy as np
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 
@@ -25,18 +23,16 @@ def run():
     tr = pd.read_csv(f"{DATA_DIR}/train.csv")
     te = pd.read_csv(f"{DATA_DIR}/test.csv")
 
-    fields = [
-        "Pclass",
-        "Sex",
-        #     'Age',
-        #     'Fare'
-    ]
+    numerical_fields = ["Pclass", "Age", "SibSp", "Parch"]
+    categorical_fields = ["Sex"]
+    fields = [*numerical_fields, *categorical_fields]
 
     X = tr[fields]
     y = tr[["Survived"]].to_numpy().reshape((len(tr),))
     preprocessor = ColumnTransformer(
         transformers=[
-            ("cat", categorical_transformer, ["Sex"]),
+            ("num", numerical_transformer, numerical_fields),
+            ("cat", categorical_transformer, categorical_fields),
         ]
     )
 
@@ -53,14 +49,12 @@ def run():
     print("Average score (across experiments):")
     print(1 - scores.mean())
 
-    # clf_final = RandomForestClassifier(max_depth=1, random_state=0)
-    # clf_final.fit(X, y)
+    my_pipeline.fit(X, y)
 
-    # y_p = clf_final.predict(get_input(te))
+    y_p = my_pipeline.predict(te[fields])
 
-    # out = pd.DataFrame({"PassengerId": te["PassengerId"], "Survived": y_p})
-
-    # out.to_csv(f"{OUTPUT_DIR}/suby.csv", index=False)
+    out = pd.DataFrame({"PassengerId": te["PassengerId"], "Survived": y_p})
+    out.to_csv(f"{OUTPUT_DIR}/out.csv", index=False)
 
 
 if __name__ == "__main__":
