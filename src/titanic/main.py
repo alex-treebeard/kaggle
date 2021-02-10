af00ebdef0695301795c51417053e9e201bf999e
alex-treebeard <alex@treebeard.io>
2021-02-10 14:37:39 +0000

    pipeline

--- src/titanic/main.py
+++ src/titanic/main.py
@@ -1,10 +1,21 @@
+import numpy as np
 import pandas as pd
+from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
 
-# from sklearn.pipeline import Pipeline
+numerical_transformer = SimpleImputer(strategy="constant")
+
+categorical_transformer = Pipeline(
+    steps=[
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("onehot", OneHotEncoder(handle_unknown="ignore")),
+    ]
+)
 
 
 def run():
@@ -16,42 +27,40 @@ def run():
 
     fields = [
         "Pclass",
+        "Sex",
         #     'Age',
         #     'Fare'
     ]
 
-    my_imputer = SimpleImputer()
-
-    def get_input(dfx):
-        s = pd.get_dummies(dfx[["Sex"]])
-        onehot = pd.concat([dfx[fields], s], axis=1)
-        return my_imputer.fit_transform(onehot)
-
-    X = get_input(tr)
-    y = tr[["Survived"]]
-
-    # my_pipeline = Pipeline(steps=[('preprocessor', SimpleImputer()),
-    #                           ('model', RandomForestClassifier(max_depth=1, random_state=0))
-    #                          ])
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.33, random_state=42
+    X = tr[fields]
+    y = tr[["Survived"]].to_numpy().reshape((len(tr),))
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("cat", categorical_transformer, ["Sex"]),
+        ]
     )
 
-    clf = RandomForestClassifier(max_depth=1, random_state=0).fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
+    my_pipeline = Pipeline(
+        steps=[
+            ("preprocessor", preprocessor),
+            ("model", RandomForestClassifier(max_depth=1, random_state=0)),
+        ]
+    )
+    scores = -1 * cross_val_score(
+        my_pipeline, X, y, cv=5, scoring="neg_mean_absolute_error"
+    )
 
-    test_score = 1 - mean_absolute_error(y_test, y_pred)
-    print(f"test score: {test_score}")
+    print("Average score (across experiments):")
+    print(1 - scores.mean())
 
-    clf_final = RandomForestClassifier(max_depth=1, random_state=0)
-    clf_final.fit(X, y)
+    # clf_final = RandomForestClassifier(max_depth=1, random_state=0)
+    # clf_final.fit(X, y)
 
-    y_p = clf_final.predict(get_input(te))
+    # y_p = clf_final.predict(get_input(te))
 
-    out = pd.DataFrame({"PassengerId": te["PassengerId"], "Survived": y_p})
+    # out = pd.DataFrame({"PassengerId": te["PassengerId"], "Survived": y_p})
 
-    out.to_csv(f"{OUTPUT_DIR}/suby.csv", index=False)
+    # out.to_csv(f"{OUTPUT_DIR}/suby.csv", index=False)
 
 
 if __name__ == "__main__":
